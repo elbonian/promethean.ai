dd23aa09251bcb798b355d90206a9000374faa1e
Samuel Reed <samdreed21@gmail.com>
2019-01-30 16:57:49 -0700

    Fixed Condition class to take name instead of value, updated condition tests

--- src/main/java/ai/promethean/DataModel/Condition.java
+++ src/main/java/ai/promethean/DataModel/Condition.java
@@ -3,66 +3,71 @@ package ai.promethean.DataModel;
 import java.lang.*;
 
 public class Condition {
-    private int val1;
-    private int val2;
+    private String name;
+    private double value;
     //this needs some work - is there any way to pass in operators directly?
     private String operator;
 
 
-    public Condition(int _val1, int _val2, String _operator){
-        //TODO: change val1 to a string and get associated value
-        setVal1(_val1);
-        setVal2(_val2);
-        setOperator(_operator);
+    public Condition(String _name, double _value, String _operator){
+        if (_operator == "==" || _operator == ">" || _operator == ">=" || _operator == "<" || _operator == "<=" || _operator == "!="){
+            setName(_name);
+            setValue(_value);
+            setOperator(_operator);
+        }
+        else{
+            //if not valid operator throw exception
+            throw new IllegalArgumentException("Illegal operator argument");
+        }
     }
 
-    public void setVal1(int val1) {
-        this.val1 = val1;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setVal2(int val2) {
-        this.val2 = val2;
+    public void setValue(double value) {
+        this.value = value;
     }
 
     public void setOperator(String operator) {
         this.operator = operator;
     }
 
-    public int getVal1() {
-        return val1;
-    }
+    public String getName() {return name; }
 
-    public int getVal2() {
-        return val2;
+    public double getValue() {
+        return value;
     }
 
     public String getOperator() {
         return operator;
     }
-    public boolean evaluate(int val1, int val2, String Operator){
-        if (operator.equals("=="))
+
+
+    public boolean evaluate(double val1){
+        if (this.operator.equals("=="))
         {
-            return val1 == val2;
+            return val1 == this.value;
         }
         else if (operator.equals(">"))
         {
-            return val1 > val2;
+            return val1 > value;
         }
         else if (operator.equals("<"))
         {
-            return val1 < val2;
+            return val1 < value;
         }
         else if (operator.equals("<="))
         {
-            return val1 <= val2;
+            return val1 <= value;
         }
         else if (operator.equals(">="))
         {
-            return val1 >= val2;
+            return val1 >= value;
         }
         else if (operator.equals("!="))
         {
-            return val1 != val2;
+            return val1 != value;
         }
         else{
             //if not valid operator throw exception
--- src/test/java/ai/promethian/DataModel/ConditionTest.java
+++ src/test/java/ai/promethian/DataModel/ConditionTest.java
@@ -7,28 +7,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ConditionTest {
-    private Condition c1 = new Condition(1, 1, "==");
-    private Condition c2 = new Condition(2, 1, ">");
-    private Condition c3 = new Condition(1, 2, "<");
-    private Condition c4 = new Condition(1, 8, "!=");
-    private Condition c5 = new Condition(2, 1, ">=");
-    private Condition c6 = new Condition(1, 2, "<=");
+    private Condition c = new Condition("Altitude", 100, ">");
+    private Condition c1 = new Condition("Fuel", 3000, "<");
+    private Condition c2 = new Condition("Fuel", 300, ">=");
 
     @Test
-    void checkEquals() { assertTrue(c1.evaluate(1, 1, "=="));}
-
+    void checkGreater() { assertTrue(c.evaluate(150));}
     @Test
-    void checkGreater() { assertTrue(c2.evaluate(2, 1, ">"));}
-
+    void checkGreaterFalse() { assertFalse(c.evaluate(50));}
     @Test
-    void checkLeast() { assertTrue(c3.evaluate(1, 2, "<"));}
-
+    void checkLess() { assertTrue(c1.evaluate(500));}
     @Test
-    void checkNotEquals() { assertTrue(c4.evaluate(1, 8, "!="));}
-
+    void checkGreaterEq() { assertTrue(c2.evaluate(500));}
     @Test
-    void checkGreaterEq() { assertTrue(c5.evaluate(2, 1, ">="));}
-
+    void checkEQ() {assertTrue(c2.evaluate(300));}
     @Test
-    void checkLessEq() { assertTrue(c6.evaluate(1, 2, "<="));}
+    void checkNQ() {assertFalse(c2.evaluate(200));}
+    
 }
