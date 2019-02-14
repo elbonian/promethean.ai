b6af7ff4352618133c5f87d3c42c7e684b556c25
bewa4021 <bewa4021@colorado.edu>
2019-02-14 14:02:50 -0700

    Creation of Test Constants class.

---
+++ src/test/java/ai/promethian/TestConstants/TestConstants.java
@@ -0,0 +1,9 @@
+package ai.promethian.TestConstants;
+
+import ai.promethean.DataModel.SystemState;
+
+public class TestConstants {
+    public static SystemState getTestState() {
+        return new SystemState(1);
+    }
+}
