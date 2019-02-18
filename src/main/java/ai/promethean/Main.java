b47d4380ee2fcf8452e0b07025bf5fffb1e1180e
TaylorJesse <taylor.jesse@colorado.edu>
2019-02-18 13:36:03 -0700

    Clean up JSON files

--- src/main/java/ai/promethean/Main.java
+++ src/main/java/ai/promethean/Main.java
@@ -1,5 +1,5 @@
 package ai.promethean;
-import ai.promethean.Parser.Parser;
+import ai.promethean.Parser.*;
 import com.github.fge.jsonschema.core.exceptions.ProcessingException;
 
 import java.io.IOException;
@@ -10,13 +10,14 @@ public class Main {
 
     public static void main(String[] args) throws IOException, ProcessingException {
 	// write your code here
-        //System.out.println("Hello World");
+        System.out.println("Hello World");
         //Input File here:
-        Parser p= new Parser("C:\\Users\\Taylor\\Desktop\\promethean.ai\\JSON input\\InputFiles\\All.json", true);
-        ArrayList<Object> objects = p.parse();
-        for(Object o: objects){
-            System.out.println(o);
-        }
+        //Parser p= new Parser("C:\\Users\\Taylor\\Desktop\\promethean.ai\\JSON input\\InputFiles\\All.json", true);
+        //Parser1 p= new Parser1("C:\\Users\\Taylor\\Desktop\\promethean.ai\\JSON input\\InputFiles\\Sample_Json_new.json", true);
+       // ArrayList<Object> objects = p.parse();
+        //for(Object o: objects){
+          //  System.out.println(o);
+       // }
 
     }
 }
