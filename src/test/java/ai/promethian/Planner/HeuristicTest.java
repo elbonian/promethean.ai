fad9f2e3f0b9a783a5b97e86b6e5e824ab73711f
bewa4021 <bewa4021@colorado.edu>
2019-02-19 22:55:26 -0700

    add start of Heuristic test file

--- src/test/java/ai/promethian/Planner/HeuristicTest.java
+++ src/test/java/ai/promethian/Planner/HeuristicTest.java
@@ -1,4 +1,19 @@
 package ai.promethian.Planner;
+import ai.promethean.DataModel.SystemState;
+import ai.promethean.DataModel.Task;
+import ai.promethean.GraphManagement.Heuristic;
+import ai.promethian.TestConstants.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HeuristicTest {
+    private SystemState fullState = StateTestConstants.getFullState();
+    private SystemState boolState = StateTestConstants.getBooleanState();
+    private Task fullTask1 = TaskTestConstants.getFullTask1();
+    private Task fullTask2 = TaskTestConstants.getFullTask2();
+
+    @Test
+    void checkH_value () {
+
+    }
 }
