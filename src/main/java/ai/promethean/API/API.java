7b744646d4f2e4f03b8899aab3016a6672156895
Samuel Reed <samdreed21@gmail.com>
2019-02-24 19:17:05 -0700

    PlannerError class created as well as API refactored to be more modular. See docstrings for adding functions to API

--- src/main/java/ai/promethean/API/API.java
+++ src/main/java/ai/promethean/API/API.java
@@ -1,45 +1,65 @@
 package ai.promethean.API;
 import ai.promethean.Parser.*;
-
-import java.io.*;
+import ai.promethean.API.*;
 import java.util.ArrayList;
+import ai.promethean.DataModel.*;
+import ai.promethean.Planner.*;
+import ai.promethean.GraphManagement.*;
+
+/*
+    This API is meant to be as simple as possible and expandable as possible.
+    If you want to add a new functionality simply add a function to the the API class.
+    PlannerError is a custom error throwing class. When throwing a PlannerError make sure to include
+    a detailed error message.
+    Example, if we want to add a function to create resource objects, simply create that function and then
+    call it with the resource object that keeps track of the examples
+
+ */
 
 public class API {
-
-    private String command;
-    private String information;
-
-    public void setCommand(String command) {
-        this.command = command;
-    }
-
-    public void setInformation(String information) {
-        this.information = information;
-    }
-
     public API(){
-        setCommand("");
-        setInformation("");
-    }
-    public API(String _command, String _information){
-        // set all commands to lowercase for safety
-        setCommand(_command.toLowerCase());
-        setInformation(_information);
+
     }
 
-    public void processCommand(){
-            // run command
-           if (this.command.equals("run")){
-               Parser p = new Parser(this.information, true);
-               ArrayList<Object> parsedObjects = p.parse();
-               System.out.println(parsedObjects);
-               //TODO MAKE THIS INSTANTIATE THE PARSER
-           }
-
-           else if (this.command.equals("error")){
-               throw new java.lang.Error(this.information);
-        }
+    public void throwError(String err_msg){
+        throw new PlannerError(err_msg);
     }
 
+    public void runSimulation(String inputFile){
+        /*
+        Example execution of graph from Hayden's Graph-Planner Branch. This is just an example of what it
+        might look like to call runSimulation, we will be continually updating this
+         */
+        /*Parser p = new Parser(inputFile, true);
+        ArrayList<Object> objects = p.parse();
+        Algorithm algo = new AStar((SystemState) objects.get(1),
+                (GoalState) objects.get(2),
+                (TaskDictionary) objects.get(3),
+                (StaticOptimizations) objects.get(0));
+
+        Planner planner = new Planner(algo);
+        Plan plan = planner.plan();
+
+        System.out.println("\nInitial State:\n======================");
+        System.out.println(plan.getInitialState());
+        System.out.println("\nRuntime Goal State:\n======================");
+        System.out.println(plan.getGoalState());
+        System.out.println("\nPlan:\n======================");
+        ArrayList<PlanBlock> list = plan.getPlanBlockList();
+        for (PlanBlock block: list) {
+            System.out.println(block.getTask());
+            System.out.println("\n");
+            System.out.println(block.getState());
+            System.out.println("\n");*/
+    }
+
+    public void generatePlan(String inputFile){
+        //TODO
+        //These currently serve as examples and only call the parser, need to link up with entire system
+        Parser p = new Parser(inputFile, true);
+        ArrayList<Object> parsedObjects = p.parse();
+        System.out.println(parsedObjects);
+    }
 
 }
+
--- src/main/java/ai/promethean/Main.java
+++ src/main/java/ai/promethean/Main.java
@@ -9,10 +9,5 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 	// write your code here
-        System.out.println("Hello World");
-        //API api = new API("run", "C:\\Users\\Sam\\Dropbox\\CSCI4308_Senior_Projects\\promethean.ai\\JSON input\\InputFiles\\Example_Json_2.json");
-        //api.processCommand();
-        API api = new API("error", "it's not working help");
-        api.processCommand();
     }
 }
