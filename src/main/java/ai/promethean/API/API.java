ce967cf1372a926b2e45bae515ef2e11912c0929
Samuel Reed <samdreed21@gmail.com>
2019-02-28 13:00:35 -0700

    API Module (#44)
    * Basic implementation of API, handles run and error commands, easily expandable
    
    * PlannerError class created as well as API refactored to be more modular. See docstrings for adding functions to API
    
    * Hello world conflicts
    
    * ParserError Class

---
+++ src/main/java/ai/promethean/API/API.java
@@ -0,0 +1,56 @@
+package ai.promethean.API;
+import ai.promethean.Parser.*;
+import java.util.ArrayList;
+import ai.promethean.DataModel.*;
+import ai.promethean.Planner.*;
+
+
+/*
+    This API is meant to be as simple as possible and expandable as possible.
+    If you want to add a new functionality simply add a function to the the API class.
+    PlannerError is a custom error throwing class. When throwing a PlannerError make sure to include
+    a detailed error message.
+    Example, if we want to add a function to create resource objects, simply create that function and then
+    call it with the resource object that keeps track of the examples
+
+ */
+
+public class API {
+    public API(){
+
+    }
+
+    public void throwPlannerError(String err_msg){
+        throw new PlannerError(err_msg);
+    }
+
+    public void throwParserError(String err_msg){
+        throw new ParserError(err_msg);
+    }
+
+    public void generatePlan(String inputFile, Boolean isFile){
+        Parser p = new Parser("JSON_input/InputFiles/test.json", isFile);
+        ArrayList<Object> objects = p.parse();
+        Algorithm algo = new AStar((SystemState) objects.get(1),
+                (GoalState) objects.get(2),
+                (TaskDictionary) objects.get(3),
+                (StaticOptimizations) objects.get(0));
+
+        Planner planner = new Planner(algo);
+        Plan plan = planner.plan();
+
+        System.out.println("\nInitial State:\n======================");
+        System.out.println(plan.getInitialState());
+        System.out.println("\nRuntime Goal State:\n======================");
+        System.out.println(plan.getGoalState());
+        System.out.println("\nPlan:\n======================");
+        ArrayList<PlanBlock> list = plan.getPlanBlockList();
+        for (PlanBlock block: list) {
+            System.out.println(block.getTask());
+            System.out.println("\n");
+            System.out.println(block.getState());
+            System.out.println("\n");
+        }
+    }
+}
+
---
+++ src/main/java/ai/promethean/API/PlannerError.java
@@ -0,0 +1,7 @@
+package ai.promethean.API;
+
+public class PlannerError extends RuntimeException {
+    public PlannerError(String message) {
+        super(message);
+    }
+}
