401baf29bff5cb9debdaa87b9b3bbaf42de52991
Evan James <ejames917@gmail.com>
2019-04-02 21:00:45 -0400

    TestCaseGenerator (#54)
    * Init of the TestCaseGenerator class. Also a slight typo fix in GoalState definition
    
    * Adding class to be able to store deltas between init and goal states. Improvement to TestCaseGenerator computPropertyDeltas method.
    
    * More work on TestCaseGenerator. Implemented generateCriticalPath method, and it seems to be working. Added a bunch of TODOs
    
    * A working test case generator. Given an input SystemState and GoalState it will generate a given number of tasks for the test case. Still needs to be expanded to reliably add requirements for tasks along with adding optimizations and perturbations, but the current implementation can be used.
    
    * Completed TestCaseGenerator! Will reliably create test cases with a specified number of tasks, perturbations, and optimizations.
    
    * Added option to not generate critical path, which makes planner do more work and doesn't garauntee a solution
    
    * Removing generated test cases from PR
    
    * Removing more generated tests, removing testing from Main.java, and adding GenerateTests.java into the test directory
    
    * Never noticed that this build was failing... Fixed it.

---
+++ src/main/java/ai/promethean/TestCaseGenerator/PropertyDelta.java
@@ -0,0 +1,22 @@
+package ai.promethean.TestCaseGenerator;
+
+public abstract class PropertyDelta {
+    protected String property_name;
+
+    public PropertyDelta() {}
+
+    public PropertyDelta(String _name) {
+        setName(_name);
+    }
+
+    public void setName(String _name) {
+        this.property_name = _name;
+    }
+
+    public String getName() {
+        return this.property_name;
+    }
+
+    public abstract Object getValue();
+
+}
