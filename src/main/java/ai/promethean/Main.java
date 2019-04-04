6a83fd05d06fca5af73e2d717d726589c8767523
Samuel Reed <samdreed21@gmail.com>
2019-04-04 12:58:49 -0600

    Removed log flag enablingin main, to reduce merge conflicts with cli

--- src/main/java/ai/promethean/Main.java
+++ src/main/java/ai/promethean/Main.java
@@ -8,8 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
         API api = new API();
-        Logger.logFlag = true;
-        Logger.printFlag = true;
         api.executePlan("JSON_input/InputFiles/PerturbationTest.json", true, "JSON_output/Plans/","JSON_output/SystemStates/");
     }
 }
