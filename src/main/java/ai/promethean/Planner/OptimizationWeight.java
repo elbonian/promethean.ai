c0907ea7fc25c8026d7bd13d3375d47d71365818
bewa4021 <bewa4021@colorado.edu>
2019-04-07 22:52:27 -0600

    Fix GitIgnore and Simplify Optimization Weighting for now to do nothing till the proper implementation is done.

--- src/main/java/ai/promethean/Planner/OptimizationWeight.java
+++ src/main/java/ai/promethean/Planner/OptimizationWeight.java
@@ -6,19 +6,8 @@ public class OptimizationWeight {
 
     private OptimizationWeight() {}
 
-    public static Double weightedPropertyValue00(Optimization optimization, Double propertyValue, int listLength) {
-        if (optimization.getType() == "min") {
-            return Math.pow(propertyValue, 2.5 + (listLength - optimization.getPriority()));
-        }
-
-        return Math.pow(2.0 + propertyValue, -1 * (2.5 + (listLength - optimization.getPriority())));
-    }
-
     public static Double weightedPropertyValue(Optimization optimization, Double propertyValue, int listLength) {
-        if (optimization.getType().equals("min")) {
-            return propertyValue; //* 1.0+optimization.getPriority();
-        } else {
-            return propertyValue; ///1.0+optimization.getPriority();
-        }
+        //TODO: Proper Optimization weighting function
+        return propertyValue;
     }
 }
